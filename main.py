bd32ef33260ad2c5ad823dfe854589d727640d3a
Lucas Rodrigues <128184954+Lusca-rcg@users.noreply.github.com>
2024-11-27 17:56:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def enviar_requests(start_number, end_number, interval):
     print("Todos os requests foram enviados.")
 
 # Parâmetros fixos (ou configure-os dinamicamente)
-start_number = 9264  # Número Inicial
-end_number = 9270   # Número Final
+start_number = 9752  # Número Inicial
+end_number = 20000   # Número Final
 interval = 10
      # Intervalo em segundos
 
